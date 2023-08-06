2cf4bbeac3dc0c0a6077a988d6884459f62de6a6
Diego Aguiar Medeiros <diego.a.medeiros@gmail.com>
2023-08-06 11:45:22 -0300

    add isPalindrome.ts

---
+++ isPalindrome.ts
@@ -0,0 +1,33 @@
+import readline from "readline";
+import { isPalindrome } from "./index.js";
+
+function askForASentence(): Promise<string> {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+
+  return new Promise((resolve, reject) => {
+    rl.question("Enter a sentence to see if it is a Palindrome: ", (answer) => {
+      rl.close();
+      resolve(answer);
+    });
+  });
+}
+
+async function main() {
+  console.log("Welcome to the factorial calculator!");
+
+  try {
+    const text = await askForASentence();
+    console.log(
+      `The sentence ${text} ${
+        isPalindrome(text) ? "is a Palindrome" : "is not a Palindrome"
+      }`
+    );
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+main();
