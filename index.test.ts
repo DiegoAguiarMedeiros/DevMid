52a10b8a0a975f67728c17b6ae0805c345f37f52
Diego Aguiar Medeiros <diego.a.medeiros@gmail.com>
2023-08-06 09:42:50 -0300

    should return 0 on 1 , 1 , -

--- index.test.ts
+++ index.test.ts
@@ -7,3 +7,7 @@ test("should accept 3 parrams", () => {
 test("should return 2 on 1 , 1 , +", () => {
   expect(calculator(1, 1, "+")).toBe(2);
 });
+
+test("should return 0 on 1 , 1 , -", () => {
+  expect(calculator(1, 1, "-")).toBe(0);
+});
--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 const calculator = (a: number, b: number, operator: "+" | "-" | "*" | "/") => {
   const operation = {
     "+": a + b,
-    "-": a + b,
+    "-": a - b,
     "/": a + b,
     "*": a + b,
   };
